19a209d7bb70b69edafabac6e16607f3755aefd6
LeivaDiego <lei21752@uvg.edu.gt>
2024-09-09 14:37:13 -0600

    Added  Variable Declaration v1
    - First iteration of the variable declaration rule semantic analysis
    
    *Curently theres no scope and symbol table...

--- src/Controller/Driver.py
+++ src/Controller/Driver.py
@@ -2,22 +2,94 @@ from Language.compiscriptVisitor import compiscriptVisitor
 from Language.compiscriptParser import compiscriptParser
 from Model.data_types import NumType, BooleanType, StringType, NilType
 
+# Semantic Analyzer
 class SemanticAnalyzer(compiscriptVisitor):
     def __init__(self):
+        # Symbol table to track variables and their types
         self.symbol_table = {}
 
-    def visitPrimary(self, ctx: compiscriptParser.PrimaryContext):
-        if ctx.NUMBER():
-            return NumType()
+
+    def visitVarDecl(self, ctx: compiscriptParser.VarDeclContext):
+        # Get the variable name
+        var_name = ctx.IDENTIFIER().getText()
+
+        # Visit the expression on the right-hand side to infer the type
+        var_type = self.visit(ctx.expression())
+
+        # Add the variable and its type to the symbol table
+        if var_name in self.symbol_table:
+            raise Exception(f"Variable '{var_name}' is already declared.")
+        else:
+            self.symbol_table[var_name] = var_type
         
+        print(f"Variable '{var_name}' declared with type '{var_type.data_type}'")
+        return var_type
+
+
+    def visitPrimary(self, ctx: compiscriptParser.PrimaryContext):
+        # Infer the type of the primary expression
+        if ctx.NUMBER():
+            return NumType()  # Could also handle floating point numbers if necessary
         elif ctx.STRING():
             return StringType()
-
-        elif ctx.getText() == 'true' or ctx.getText() == 'false':
+        elif ctx.IDENTIFIER():
+            var_name = ctx.IDENTIFIER().getText()
+            if var_name in self.symbol_table:
+                return self.symbol_table[var_name]
+            else:
+                raise Exception(f"Undeclared variable '{var_name}'")
+        elif ctx.getText() == "true" or ctx.getText() == "false":
             return BooleanType()
-        
-        elif ctx.getText() == 'nil':
+        elif ctx.getText() == "nil":
             return NilType()
-        
         else:
-            return self.visit(ctx.expression())
+            return self.visitChildren(ctx)
+
+
+    def visitExpression(self, ctx: compiscriptParser.ExpressionContext):
+        # Handle expressions leading to assignments or function calls
+        return self.visit(ctx.assignment())
+
+
+    def visitAssignment(self, ctx: compiscriptParser.AssignmentContext):
+        # Visit the logic or (expression after the assignment)
+        return self.visit(ctx.logic_or())
+
+
+    def visitLogic_or(self, ctx: compiscriptParser.Logic_orContext):
+        # Visit the logical and expression
+        return self.visit(ctx.logic_and(0))
+
+    def visitLogic_and(self, ctx: compiscriptParser.Logic_andContext):
+        # Visit the equality expression
+        return self.visit(ctx.equality(0))
+
+
+    def visitEquality(self, ctx: compiscriptParser.EqualityContext):
+        # Visit the comparison expression
+        return self.visit(ctx.comparison(0))
+
+
+    def visitComparison(self, ctx: compiscriptParser.ComparisonContext):
+        # Visit the term expression
+        return self.visit(ctx.term(0))
+
+
+    def visitTerm(self, ctx: compiscriptParser.TermContext):
+        # Visit the factor expression
+        return self.visit(ctx.factor(0))
+
+
+    def visitFactor(self, ctx: compiscriptParser.FactorContext):
+        # Visit the unary expression
+        return self.visit(ctx.unary(0))
+
+
+    def visitUnary(self, ctx: compiscriptParser.UnaryContext):
+        # Visit the primary expression
+        return self.visit(ctx.call())
+
+    
+    def visitCall(self, ctx: compiscriptParser.CallContext):
+        # Visit the primary expression
+        return self.visit(ctx.primary())
--- src/main.py
+++ src/main.py
@@ -3,17 +3,24 @@ from antlr4 import *
 from Model.parse_tree import TreeVisualizer
 from Language.compiscriptLexer import compiscriptLexer
 from Language.compiscriptParser import compiscriptParser
+from Controller.Driver import SemanticAnalyzer
 
 def main():
-    input_path = 'src/Input/suma_simple.cspt' 
+    # Path to the input file
+    input_path = 'src/Input/var_decl.cspt' 
 
+    # Create an input stream from the input file
     input_stream = FileStream(input_path)
 
+    # Create a lexer and parser for the input stream
     lexer = compiscriptLexer(input_stream)
     stream = CommonTokenStream(lexer)
     parser = compiscriptParser(stream)
 
-    tree = parser.program()
+    tree = parser.program() # Start rule is 'program'
+
+    # Create a parse tree visualizer and visit the parse tree
+    # to generate a PNG file of the parse tree
     visualizer = TreeVisualizer()
     visualizer.visit(tree)
 
@@ -24,6 +31,11 @@ def main():
     output_file_name = f"parse_tree_{file_name}"
 
     visualizer.render(output_file=output_file_name, format='png', output_dir='src/Output')
+
+    # Create a semantic analyzer and visit the parse tree
+    analyzer = SemanticAnalyzer()
+    # Visit the parse tree to perform semantic analysis
+    analyzer.visit(tree)
 
 if __name__ == '__main__':
     main()
